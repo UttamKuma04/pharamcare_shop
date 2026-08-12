f5b48228603efbdaa636cc72d3b4defecc1fb57e
UttamKuma04 <UttamKuma04@users.noreply.github.com>
2026-08-12 13:11:37 +0530

    Celery worker1

--- mr_doctor/settings.py
+++ mr_doctor/settings.py
@@ -243,9 +243,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #<<<<<<<<<<<<<<<SMTP SET-UP>>>>>>>>>>>>>>>>>>
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = int(os.getenv('EMAIL_TIMEOUT', '10'))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '465'))
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'True') == 'True'
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_TIMEOUT = int(os.getenv('EMAIL_TIMEOUT', '20'))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER or "no-reply@pharmacare.local")
